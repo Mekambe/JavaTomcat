da2119cace6251ab2b9d8e0749ad456b1e8b31c3
JMisiorny <mekambe2@gmail.com>
2018-11-13 20:31:38 +0100

    Kalkulator i powitanie

---
+++ src/main/java/servlets/CalculatorServlet.java
@@ -0,0 +1,83 @@
+package servlets;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+resp.addHeader("Content-Type", "text/html");
+        Integer a = maptoInt(req.getParameter("a"));
+        Integer b = maptoInt(req.getParameter("b"));
+        String operation = req.getParameter("operation");
+String message = req.getParameter("message");
+
+
+        PrintWriter writer = resp.getWriter();
+        createMessage (message, writer);
+        createForm(writer);
+writer.print("Result: ");
+
+        if ("+".equals(operation)){
+            writer.println(a + b);
+
+        }else if ("-".equals(operation)){
+            writer.println(a - b);
+
+        }else if ("*".equals(operation)){
+            writer.println(a * b);
+        }else if ("/".equals(operation)){
+         if (b==0) {
+
+
+
+             resp.sendRedirect(req.getContextPath() + req.getServletPath() +  "?message=Nie Dziel przez zero");
+
+         }else {
+             writer.println((double) a/b);
+         }
+        }
+
+    }
+
+    private void createMessage(String message, PrintWriter writer) {
+        if (message != null) {
+            writer.println("<p style=\"color: red;\">" + message + "</p>");
+
+        }
+    }
+
+    private void createForm(PrintWriter writer) {
+        writer.println("<form action=\"\">");
+        writer.println("<input type=\"number\" name=\"a\">");
+        writer.println("<select name=\"operation\">");
+        writer.println("<option value=\"+\">Dodawanie</option>");
+        writer.println("<option value=\"-\">Odejmowanie</option>");
+        writer.println("<option value=\"*\">Mnozenie</option>");
+        writer.println("<option value=\"/\">Dzielenie</option>");
+        writer.println("</select>");
+        writer.println("<input type=\"number\" name=\"b\">");
+        writer.println("<input type=\"submit\" value=\"=\">");
+        writer.println("</form>");
+
+
+
+    }
+
+    private Integer maptoInt (String value){
+        if (value == null || StringUtils.isNumeric(value)){
+
+            return 0;
+        }
+
+        return Integer.valueOf(value);
+    }
+
+}
